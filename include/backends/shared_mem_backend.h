7dc625d6d9d1131a01be4a349dc51534ddb802e9
McCabe, Robert J <Robert.McCabe@rockwellcollins.com>
2015-08-10 09:43:47 -0500

    better requirements

---
+++ include/backends/shared_mem_backend.h
@@ -0,0 +1,59 @@
+#include <boost/interprocess/shared_memory_object.hpp>
+namespace rc
+{
+namespace stat_log
+{
+   //TODO: templates?
+   class shared_mem_backend
+   {
+      public:
+         setParams(const std::string& name, size_t size
+      private:
+         std::shared_ptr<boost::interprocess::shared_memory_object> shm_ptr;
+   };
+
+}
+}
+
+
+#if 0
+#include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/interprocess/mapped_region.hpp>
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace boost::interprocess;
+using std::chrono::seconds;
+
+int main(int argc, char** argv)
+{
+   using namespace std;
+   cout << argc << std::endl;
+   std::string name {argv[1]};
+   shared_memory_object shm_obj
+      (open_or_create
+       ,name.c_str()
+       ,read_write                   //read-write mode
+      );
+
+   shm_obj.truncate(1000);
+
+   mapped_region region{shm_obj, read_write};
+
+   char* ptr = (char*)region.get_address();
+
+   while(1)
+   {
+      cout << "Pattern\n";
+      for(int i = 0; i < 100; ++i)
+         ptr[i] = i;
+      std::this_thread::sleep_for(seconds(1));
+      cout << "Zeros\n";
+      std::memset(ptr, 0, 100);
+      std::this_thread::sleep_for(seconds(1));
+   }
+   return 0;
+}
+#endif
+
---
+++ test/test.cpp
@@ -0,0 +1,6 @@
+#include "rc_stat_log.h"
+
+
+using namespace rc_stat_log;
+
+struct

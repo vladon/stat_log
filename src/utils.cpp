0ad3473124cc5b821d3680bf3f01e1998de988fb
McCabe, Robert J <Robert.McCabe@rockwellcollins.com>
2015-09-27 16:44:20 -0500

    Added short options, and removed unnecessary tokenization

--- src/utils.cpp
+++ src/utils.cpp
@@ -2,11 +2,6 @@
 #include <stat_log/util/utils.h>
 #include <stat_log/util/printer.h>
 
-#include <boost/tokenizer.hpp>
-#include <boost/algorithm/string/join.hpp>
-#include <boost/token_functions.hpp>
-#include <boost/algorithm/string.hpp>
-
 #include <boost/program_options.hpp>
 #include <boost/program_options/options_description.hpp>
 #include <boost/program_options/parsers.hpp>
@@ -20,29 +15,6 @@
 #define TERM_NUM_COLUMNS 100
 
 namespace po = boost::program_options;
-namespace
-{
-std::vector<std::string> tokenize(const std::string& input)
-{
-   typedef boost::escaped_list_separator<char> separator_type;
-   separator_type separator("\\",     // The escape characters.
-         "= ",     // The separator characters.
-         "\"\'");  // The quote characters.
-
-   // Tokenize the intput.
-   boost::tokenizer<separator_type> tokens(input, separator);
-
-   // Copy non-empty tokens from the tokenizer into the result.
-   std::vector<std::string> result;
-   copy_if(tokens.begin(), tokens.end(), std::back_inserter(result),
-         [](auto& str)
-         {
-         return !str.empty();
-
-         });
-   return result;
-}
-}
 
 namespace stat_log
 {
@@ -50,48 +22,50 @@ po::options_description getProgramOptions()
 {
    po::options_description desc("Options", TERM_NUM_COLUMNS);
    desc.add_options()
-   ("help", po::value<std::string>()->implicit_value(""),
+   ("help,h", po::value<std::string>()->implicit_value(""),
     "Show help menu.\n")
 
-   ("show-tags", po::bool_switch()->default_value(false),
+   ("show-tags,s", po::bool_switch()->default_value(false),
     "Show the tag hierarchy.\n")
 
-   ("tags", po::value<std::vector<std::string>>()->multitoken()->zero_tokens(),
-    "Limit the output to the descendants of the given tag(s).\n"
+   ("tags,t", po::value<std::vector<std::string>>()->multitoken()->zero_tokens(),
+    "Limit the effects to the descendants of the given tag(s).\n"
     "Delimit multiple tags with a space.\n"
     "\".\" should be used when specifying hierarchical tags.\n")
 
-   ("dump-stats", po::bool_switch()->default_value(false),
+   //TODO: add option if TimeSeries statistics are to be dumped as well.
+   ("dump-stats,d", po::bool_switch()->default_value(false),
     "Dump all statistics.\n")
 
    ("stat-types", po::bool_switch()->default_value(false),
     "Print the type of each statistic.\n")
 
-   ("clear-stats", po::bool_switch()->default_value(false),
+   ("clear-stats,c", po::bool_switch()->default_value(false),
     "Zero out statistics.\n")
 
-   ("log-level", po::value<std::vector<std::string>>()->multitoken()->zero_tokens(),
+   ("log-level,L", po::value<std::vector<std::string>>()->multitoken()->zero_tokens(),
     "Set/Show per tag log level. Args\n"
     "\t<loggerIdx> [<LogLevel>]\n"
     "where loggerIdx is the index of the logger, and LogLevel is\n"
     "the new value for the log level (if this argument is not\n"
     "specified the current log level is displayed).\n")
 
-   ("output-log", po::value<std::vector<std::string>>()->multitoken()->zero_tokens(),
-    "type \"output-log --help\" for all options.\n")
+   ("output-log,D", po::value<std::vector<std::string>>()->multitoken()->zero_tokens(),
+    "type \"output-log --help\" for all options.\n"
+    "This option is NOT compatible with the --tags option.\n")
    ;
    return desc;
 }
 
 
-std::vector<std::string> getTagNames(const std::string& cmd_line)
+std::vector<std::string> getTagNames(int argc, char** argv)
 {
    namespace po = boost::program_options;
    std::string tag_str;
    auto desc = getProgramOptions();
 
    po::variables_map vm;
-   po::store(po::command_line_parser(tokenize(cmd_line))
+   po::store(po::command_line_parser(argc, argv)
          .options(desc)
          .allow_unregistered()
          .run(),
@@ -111,19 +85,14 @@ void parseCommandLineArgs(int argc, char** argv,
    //TODO: fill in the print_options arg.
    //  --indices, --table, etc.
    cmd = StatCmd::NO_CMD;
-   std::vector<std::string> user_strings;
-   for(int i = 1; i < argc; ++i)
-      user_strings.push_back(argv[i]);
-
-   std::string user_cmd_line = boost::algorithm::join(user_strings, " ");
-   auto tags = getTagNames(user_cmd_line);
+   auto tags = getTagNames(argc, argv);
    tag_strings.insert(tag_strings.end(), tags.begin(), tags.end());
    if(tag_strings.empty())
       tag_strings.push_back("");
 
    auto desc = getProgramOptions();
    po::variables_map vm;
-   auto parsed = po::command_line_parser(tokenize(user_cmd_line))
+   auto parsed = po::command_line_parser(argc, argv)
          .options(desc)
          .allow_unregistered()
          .run();
